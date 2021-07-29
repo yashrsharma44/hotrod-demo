feb0b5677e11b03d482dc750a719aed0ed3ebcf1
Yash Sharma <yashrsharma44@gmail.com>
2021-07-29 12:45:26 +0530

    Initial Commit
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

---
+++ main.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2019 The Jaeger Authors.
+// Copyright (c) 2017 Uber Technologies, Inc.
+
+package main
+
+import "github.com/yashrsharma44/hotrod/cmd"
+
+func main() {
+	cmd.Execute()
+}
